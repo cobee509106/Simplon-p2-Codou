c56b5c84142d4c5685ac7aae2829d4c4d18bfb6a
Codou <codouniang15@gmail.com>
2019-04-11 15:14:48 +0200

    exercice aléatoire

--- n_aleatoire2.py
+++ n_aleatoire2.py
@@ -6,9 +6,9 @@ while user_answer > 0:
      user = int(input("Veuillez entrer un nombre compris entre 0 et 9: "))
      if user == random_user:
            print ("Bravo!tu as choisi le bon numéro")
+           break
      elif user != random_user:
            print ("Opss! mauvais numéro reéssayer")
-           break
      user_answer -= 1
 else:
     print ("Désolé, vous avez échoué 3 fois!")  
