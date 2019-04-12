a416fd4c473f4b3edc50aca66c36c850ba59320a
Codou <codouniang15@gmail.com>
2019-04-12 11:22:19 +0200

    exercice aléatoire suite

--- n_aleatoire2.py
+++ n_aleatoire2.py
@@ -1,5 +1,5 @@
 from random import randint
-random_user = randint(0, 9)
+random_user = randint(0 , 9)
 user_answer = 3
 while user_answer > 0:
 
@@ -8,7 +8,7 @@ while user_answer > 0:
            print ("Bravo!tu as choisi le bon numéro")
            break
      if user > random_user:
-           print ("vous êtes sorti de l'intervalle 1 et 9")
+           print ("vous êtes sorti de l'intervalle 0 et 9")
      elif user != random_user:
             print ("Opss! mauvais numéro reéssayer")  
      user_answer -= 1
