538cfc29a036daa78211273a95177e1b81a2230f
codou <codouniang15@gmail.com>
2019-05-16 11:07:35 +0000

    Mes projets(evaluation1)

--- prairie/n_aleatoire1.py
+++
@@ -1,7 +0,0 @@
-from random import randint
-x = randint(0,9)
-user_answer= int(input("Veuillez entrer un nombre compris entre 0 et 9 "))
-if user_answer == x :
-    print("Bravo, you Win!!")
-else:
-    print("Ouuupss!, sorry!")
